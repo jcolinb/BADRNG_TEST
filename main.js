5824c30b132e1bd9cdff1a931755b7169e93fb4d
c mulligan <gateless@protonmail.com>
2020-05-21 16:22:25 -0400

    add resolving JSON Promise in fetch

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 import {state,acts,pipe,Nomad} from './Beatnik.js'
 import {list} from './dredge.js'
 
-const parse_response = pipe((r)=> r.json(),({data})=>data[0],({image_uris})=>image_uris,({png})=>document.getElementById('card').style.backgroundImage = `url(${png})`)
+const parse_response = pipe(({data})=>data[0],({image_uris})=>image_uris,({png})=>document.getElementById('card').style.backgroundImage = `url(${png})`)
 
-const load_card = (nom) => fetch(`https:\/\/api.scryfall.com/cards/search?q=${nom.replace(/( )/gi,'+')}`).then(parse_response)
+const load_card = (nom) => fetch(`https:\/\/api.scryfall.com/cards/search?q=${nom.replace(/( )/gi,'+')}`).then((r)=>r.json()).then(parse_response)
 
 const build_list = ({nom,num}) => {
   const item = document.createElement('div');
